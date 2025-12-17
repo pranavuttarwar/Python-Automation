a88e0309f21fcb9c6d2ec11c61401ad731fc16d5
Pranav Uttarwar <uttarwar,pranav@gmail.com>
2025-12-17 21:39:16 +0530

    Old and new

---
+++ TypesofVaribale/GlobalVariable.py
@@ -0,0 +1,20 @@
+#1 Global Variable
+
+num=10
+
+print(num)
+
+print("----")
+
+def demo():
+    print(num)
+
+demo()
+print("----")
+
+class demo1:
+    def acc1(self):
+        print(num)
+
+i=demo1()
+i.acc1()
---
+++ TypesofVaribale/LocalVariable.py
@@ -0,0 +1,15 @@
+#2 Local Varibale
+
+def demo():
+    a=10
+    b=10
+    print(a+b)
+
+demo()
+
+print("-------")
+
+def demo1(a,b):
+    print(a+b)
+
+demo1(10,20)
