ff13d0aca1bc0d899f40319c27966e534494b938
Pranav Uttarwar <uttarwar,pranav@gmail.com>
2025-12-18 21:39:21 +0530

    Old and new

---
+++ Module And Pacakages/Calculator.py
@@ -0,0 +1,15 @@
+
+num=10
+def add(a,b):
+    print(a+b)
+
+def mult(a,b):
+    print(a*b)
+
+class demo:
+    num1=100
+    def m1(self):
+        print("M1 method form demo class")
+
+    def m2(self):
+        print("M2 method form demo class")
